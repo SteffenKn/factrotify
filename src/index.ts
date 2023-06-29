f5fc0fbc3e8a370819931bead76dda96c2aa5dac
Steffen <Steffenknaup@hotmail.de>
2023-06-29 17:48:54 +0200

    Fix naming

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
 
-import { registerIocModule } from './IocModule';
+import { registerIocModule } from './ioc-module';
 import { Webserver } from './webserver';
 import { SlackClient } from './clients';
 
